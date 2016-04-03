e08a5816baa23ea8aa058ceeda9c1522cb3a5a62
meg matty <meghopweb@me.com>
2016-04-03 12:21:49 -0700

    Updated CSS & JS
    backup of working changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //OVERLAY
 
-	// Add overlay variable
+	// Add overlay, image, caption variables
 	var $overlay = $('<div id="overlay"></div>');
 	var $image = $("<img>");
 	var $caption = $("<p></p>");
@@ -16,7 +16,6 @@
 	
 	
 	// Capture click event on a link to an image
-	
 	$("#imageGallery a").click(function(event) {
 		event.preventDefault(); //prevents link from opening in dead end
 		var imageLocation = $(this).attr("href"); //gets link attribute in variable
@@ -33,53 +32,54 @@
 	});
 	
 	// When overlay is clicked
-	
 	$overlay.click(function() {
 		// Hide/fade out overlay 
 		$overlay.fadeOut(500);
 	});
 
 
+//SEARCH
+
+//When text typed in Search box
+	$("#search").keyup(function() {
+			//Take the input and store it in var filter
+			var filter= $(this).val();
+				//Check each image in the gallery
+			$("#imageGallery img").each(function() {
+					//If the alt text of the image doesn't match (< 0) the typed input
+					//"gi" means global(all matches), case insensitive
+					if ($(this).attr("alt").search(new RegExp(filter, "gi")) < 0 ) {
+					
+						//Take all parent elements of img to the ul container (li, a)
+						$(this).parentsUntil("#imageGallery").fadeOut(1000); //Fade the item out
+						
+					} else if ($("html").width() < 1024) { //if the viewport is <1024
+						$(this).parentsUntil("#imageGallery").fadeIn(1000); //Fade it in
+						
+					} else { // viewport is > 1024
+					  	$(this).parentsUntil("#imageGallery").css("margin-right", "60px").fadeIn(1000); //Fade it in with new right margin
+					}
+					
+	});				
+							
+			//If the search box is empty and is >= 1024 viewport			
+			if ($("#search").val().length < 1 && $("html").width() >= 1024) {
+			
+					//Restore right margins to original values
+					$("#imageGallery li").css("margin-right", "60px");
+					$("#imageGallery li:nth-child(4n)").css("margin-right", 0);
+					
+				} 						
+			});	
+		
+
+//TO DO
+
 //Forward/backward arrows
 	//When clicked, arrows show image prev/next in list
 	//When R/L arrow keys clicked, show image prev/next in list
 
-
-
-
-
-
-//SEARCH
-	
-	//When text typed in Search box
-		$("#search").keyup(function() {
-		//Take the input and store it in var filter
-			var filter= $(this).val();
-			//Check each image in the gallery
-			$("#imageGallery img").each(function() {
-				//If the alt text of the image doesn't match (< 0) the typed input
-				//"gi" means global(all matches), case insensitive
-				if ($(this).attr("alt").search(new RegExp(filter, "gi")) < 0 ) {
-					//Take the img up to its parent item
-					$(this).parentsUntil("#imageGallery").fadeOut(1000); //Fade the item out
-				} else { //Fade item in
-					$(this).parentsUntil("#imageGallery").css("margin-right", "60px").fadeIn(1000); //Fade it in and adjust margin
-				}
-				
-				});
-			//ONLY FIRE THIS OFF IF media size min-width 1024px	
-			//if the search box is empty, set margins to the way they were before
-			if ($("#search").val().length < 1) {
-					$("#imageGallery li").css("margin-right", "60px");
-					$("#imageGallery li:nth-child(4n)").css("margin-right", 0);
-				}
-			});
-	
-		//Animate hiding and reordering of photos
-
-
-
-
-
-
-
+		
+		
+		
+			
