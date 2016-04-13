c4d56f5d87a502d3302698452e330530ecfb1fa4
meg matty <meghopweb@me.com>
2016-04-12 20:08:38 -0700

    JS rework, CSS for videos
    Reworked JS so that image changing is taken care of by a single
    function, then call that both when clicking on a thumbnail as well as
    navigating.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 	var $overlay = $('<div id="overlay"></div>');
 	var $image = $("<img>");
 	var $caption = $("<p></p>");
-	var $iframe = $('<iframe src="" frameborder="0"></iframe>'); //for video holder?
+	var $iframe = $('<iframe src="" frameborder="0"></iframe>'); //empty video holder
 	
 	// Counter variable for arrows
 	var $index = 0;
@@ -16,7 +16,8 @@
 	//Append image to overlay
 	$overlay.append($image);
 	
-//	$overlay.append($iframe); i guess append the iframe here? or append in if statement
+	//Append iframe to overlay
+	$overlay.append($iframe); 
 	
 	//Append buttons to overlay
 	$overlay.append($prevArrow);
@@ -28,136 +29,138 @@
 	// Append overlay to body
 	$("body").append($overlay);
 	
+	
+	//Create a function to show the desired gallery item
+	function showGalleryItem(item) {
+	
+	    var item = $(item); //check to see if i need this
+	    
+	    if (item.hasClass("video") ) { //if the item has a class 'video'
+	        var showItem = $iframe; //define variable to show the iframe
+	        var hideItem = $image; //define variable to hide the image
+	    } else { //if the item is not a video
+	        var showItem = $image; //define variable to show the image
+	        var hideItem = $iframe;//define variable to hide the iframe
+	    }
+	
+	    hideItem.hide(); //hide the items based on the met condition
+	    showItem.show().fadeOut(0).fadeIn(1000); //show the items and fadeout quick then fade back in slower
+	
+	    showItem.attr("src", item.attr('href')); //set the src of the item to the link
+	
+	    //Update the index to the current image
+	    $index = item.parent().index();
+	    
+	    //Get child img alt attribute and set to caption
+	    var captionText = item.children("img").attr("alt");
+	    $caption.text(captionText);
+	    
+	    //Fade in the overlay
+	    $overlay.fadeIn(500);
+	}
+	
+	
 	// Capture click event on a link to an image
 	$("#imageGallery a").click(function(event) {
 		event.preventDefault(); //prevents link from opening in dead end
 		
-		var imageLocation = $(this).attr("href"); //gets img link attribute in variable
-//		var videoLocation = $(this).attr("href"); //gets video link attr? but its the same?
-        
-//      $iframe.attr("src", imageLocation); //?????no idea if this is right
+        var item_to_show = $(this); //get THIS link in a variable
+		
+		showGalleryItem(item_to_show); //call function
 
-    // Update the overlay with the image linked in the link
-            $image.attr("src", imageLocation);      
-        
-           
-        //where i get stuck - no idea how to implement video with my existing code
-        //add a class ".video" to video a elements
-        //if $(this).hasClass("video") then $iframe.append to overlay
-        //else do the rest of code?
-
-		//Update the index to the current image
-		$index = $(this).parent().index(); 
-		
-		// Show/fade in the overlay
-		$overlay.fadeIn(500); 
-		
-		// Get child img alt attribute and set to caption
-		var captionText = $(this).children("img").attr("alt");
-		$caption.text(captionText);
-		
 	});
+	
 	
 //SEARCH
 
-//When text typed in Search box
-	$("#search").keyup(function() {
-	//Take the input and store it in var filter
-		var filter= $(this).val();
-		//Check each image in the gallery
-		$("#imageGallery img").each(function() {
-			//If the alt text of the image doesn't match (< 0) the typed input
-			//"gi" means global(all matches), case insensitive
-			if ($(this).attr("alt").search(new RegExp(filter, "gi")) < 0 ) {
-			
-				//Select all parent elements of img (li, a)
-				$(this).parents("li").fadeOut(1000); //Fade the item out
-				
-			} else {
-			  	$(this).parents("li").addClass('filtered').fadeIn(1000); //Fade it in with new right margin (added by .filtered)
-			}
-			//If the search box is empty			
-			if ( $("#search").val().length < 1) {
-				$(this).parents("li").removeClass('filtered'); //remove the .filtered class to restore margins to normal
-			} 						
-		});				
-	
-	});	
+    //When text typed in Search box
+    	$("#search").keyup(function() {
+    	//Take the input and store it in var filter
+    		var filter= $(this).val();
+    		//Check each image in the gallery
+    		$("#imageGallery img").each(function() {
+    			//If the alt text of the image doesn't match (< 0) the typed input
+    			//"gi" means global(all matches), case insensitive
+    			if ($(this).attr("alt").search(new RegExp(filter, "gi")) < 0 ) {
+    			
+    				//Select all parent elements of img (li, a)
+    				$(this).parents("li").fadeOut(1000); //Fade the item out
+    				
+    			} else {
+    			  	$(this).parents("li").addClass('filtered').fadeIn(1000); //Fade it in with new right margin (added by .filtered)
+    			}
+    			//If the search box is empty			
+    			if ( $("#search").val().length < 1) {
+    				$(this).parents("li").removeClass('filtered'); //remove the .filtered class to restore margins to normal
+    			} 						
+    		});				
+    	
+    	});	
 
-//PREV_NEXT ARROW NAVIGATION - needs better fades
-var $galleryLength = $("#imageGallery li").length; //holds the length of the gallery list items
 
-var imgUpdate = function(prev) { //function for updating the overlay img
 
-	if(!prev) { //if function not true, iterate forwards
-	    $index++; 
-	} 
-	    else {  //otherwise, iterate backwards
-	        $index--; 
-	    } 
+//PREV_NEXT ARROW NAVIGATION
 
-	if ($index < 0) { //sets correct index when going backward
-	    $index = $galleryLength - 1;	
-	} 
-	
-	if ($index >= $galleryLength) { //loops back to first image
-	    $index = 0;	
-	} 
+    var $galleryLength = $("#imageGallery li").length; //holds the length of the gallery list items
+    
+    var imgUpdate = function(move) { //function for updating the overlay img when prev/next clicked
+    
+        $index += move;
+    
+    	if ($index < 0) { //sets correct index when going backward
+    	    $index = $galleryLength - 1;	
+    	} 
+    	
+    	if ($index >= $galleryLength) { //loops back to first image when end is reached
+    	    $index = 0;	
+    	} 
+    
+    	var item_to_show = $("#imageGallery li a").eq($index); //selects an item with the current index number	
+    
+        showGalleryItem(item_to_show);
 
-	var newImg = $("#imageGallery li").get($index).getElementsByTagName('a'); //gets anchor tag
-	
-	var imageLocation = $(newImg).attr("href"); //holds location for new img
-	var captionText = $(newImg).children("img").attr("alt"); //holds caption for new img
-	
-	$image.fadeOut(50); //find better way to fade, this is a fake
-	
-	$image.attr("src", imageLocation); //set new image in overlay
-	
-	$image.fadeIn(1000); //find better way to fade
-	
-	$caption.text(captionText); //set new caption text in overlay
+        };
 
-};
 
 //OVERLAY IMAGE NAVIGATION
 
-//When prevArrow is clicked
-$prevArrow.click(function(event) {
-	imgUpdate(true);
-	return false; //keeps overlay from closing when clicking arrow
-});
-
-//When nextArrow is clicked
-$nextArrow.click(function(event) {
-	imgUpdate();
-	return false; //keeps overlay from closing when clicking arrow
-});		
-
-//Arrow keys for prev/next
-$(document).keydown(function(event) {
-    if (event.which === 37) {
-        imgUpdate(true);
-    } else if (event.which === 39) {
-        imgUpdate();
-    }
-});
-
-//Mobile swipe events
-$('#overlay img').swipeleft(function(event) {
-    imgUpdate(true);
-});
-
-$('#overlay img').swiperight(function(event) {
-    imgUpdate();
-});
-
-//EXIT OVERLAY BY CLICKING ON IT
-
-//	 When overlay is clicked
-	$overlay.click(function() {
-		//Hide/fade out overlay 
-		$overlay.fadeOut(500);
-	});	
+    //When prevArrow is clicked
+    $prevArrow.click(function(event) {
+    	imgUpdate(-1); //move -1
+    	return false; //keeps overlay from closing when clicking arrow
+    });
+    
+    //When nextArrow is clicked
+    $nextArrow.click(function(event) {
+    	imgUpdate(1); //move +1
+    	return false; //keeps overlay from closing when clicking arrow
+    });		
+    
+    //Arrow keys for prev/next
+    $(document).keydown(function(event) {
+        if (event.which === 37) { //keycode for left arrow key
+            imgUpdate(-1); //move -1
+        } else if (event.which === 39) { //keycode for right arrow key
+            imgUpdate(1); //move +1
+        }
+    });
+    
+    //Mobile swipe events
+    $('#overlay img').swipeleft(function(event) {
+        imgUpdate(-1); //move -1
+    });
+    
+    $('#overlay img').swiperight(function(event) {
+        imgUpdate(1); //move +1
+    });
+    
+    //EXIT OVERLAY BY CLICKING ON IT
+    
+    //	 When overlay is clicked
+    	$overlay.click(function() {
+    		//Hide/fade out overlay 
+    		$overlay.fadeOut(500);
+    	});	
 
 
 
