76a374b34641332a7be0d89e2b01ba7e3a367caf
meg matty <meghopweb@me.com>
2016-04-13 19:00:02 -0700

    image preload function
    testing a function to preload images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+//Preload images *CSS TRICKS*
+$.preloadImages = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    $("<a>").attr("href", arguments[i]);
+  }
+}
+
+$.preloadImages("01.jpg","02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg","10.jpg", "11.jpg", "12.jpg");
+
+
 //OVERLAY
 
 	// Add overlay, image, caption variables
