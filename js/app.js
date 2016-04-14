72741ecb231680b4fd14a9e57dcad4e40e6af97c
meg matty <meghopweb@me.com>
2016-04-13 21:40:05 -0700

    preloader plugin
    implemented a preloaded plugin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+//PRELOAD IMAGES - PLUGIN
+
+$.preload( '/img/01.jpg',
+        '/img/02.jpg',
+        '/img/03.jpg',
+        '/img/04.jpg',
+        '/img/05.jpg',
+        '/img/06.jpg',
+        '/img/07.jpg',
+        '/img/08.jpg',
+        '/img/09.jpg',
+        '/img/10.jpg',
+        '/img/11.jpg',
+        '/img/12.jpg',
+      );
+
 //OVERLAY
 
 	// Add overlay, image, caption, iframe variables
